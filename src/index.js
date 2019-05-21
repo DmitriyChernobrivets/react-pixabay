07e0cd5787528d90189257f55075c39c4c3b7d50
DmitriyChernobrivects <mezete10@ukr.net>
2019-05-21 13:15:06 +0300

    connected to saga

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
+const sagaMiddleware = createSagaMiddleware();
+// mount it on the Store
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+// then run the saga
+sagaMiddleware.run(mySaga);
+
+// render the application
 ReactDOM.render(<App />, document.getElementById("root"));
